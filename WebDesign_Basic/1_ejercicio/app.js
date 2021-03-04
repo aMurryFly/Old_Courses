c6bace607bf44705f0517970a6cd18a89c35f936
Alfonso Murrieta <alfonsomvmx@outlook.com>
2021-03-03 22:01:40 -0600

    web design course

---
+++ WebDesign_Basic/1_ejercicio/app.js
@@ -0,0 +1,64 @@
+ /////////////////////////////////////////////////////->1)
+ 
+ //document.write("<h1>This is my first example of JS</h1>");
+
+
+ /////////////////////////////////////////////////////->2)
+
+ //TIPOS DE DATOS
+
+
+ //Los mismos de siempre
+
+ /*Objetos 
+
+ 
+
+ {
+    "username": 'Alfonso',
+    "score": 70.4,
+    "hours": 14
+    "professional": true
+//PARES CLAVES: VALOR
+ }
+ 
+ 
+ */
+
+ /////////////////////////////////////////////////////->3)
+
+//nameUser = "Alfonso"; también es válido
+//También se puede realizar const     
+
+
+/*
+
+var nameUser = "Alfonso";
+let LastName = "Murrieta";
+
+console.log(nameUser);
+console.log(LastName);
+
+
+*/
+
+ /////////////////////////////////////////////////////->4)
+
+
+let num1= 13.4;
+let num2= 45.4;
+
+let result = num1 != num2;
+console.log(result);
+
+var nameUser = "Alfonso";
+let LastName = "Murrieta";
+let completeName = nameUser+LastName; 
+
+console.log(completeName);
+
+
+
+
+
+
